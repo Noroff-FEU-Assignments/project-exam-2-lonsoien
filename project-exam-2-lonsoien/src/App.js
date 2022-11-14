5db1ba17a1978cb85abae9b5b6134afd8b58b9e6
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-11-14 20:09:45 +0100

    creating routes

--- project-exam-2-lonsoien/src/App.js
+++ project-exam-2-lonsoien/src/App.js
@@ -1,13 +1,34 @@
 import './sass/style.scss';
 import Home from "./components/home/Home"; 
 import Menu from "./components/layout/Menu"; 
+import SykkelcontainerFolie from './components/products/SykkelcontainerFolie';
+import SykkelcontainerTrepanel from './components/products/SykkelcontainerTrepanel';
+import Habitatpanel from './components/products/Habitatpanel';
+import Trepanel from './components/products/Trerampe';
+import Projects from './components/projects/Projects';
+import About from './components/about/About';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Container from 'react-bootstrap/esm/Container';
 
 function App() {
   return (
     <>
+    <Router>
     <Menu />
-    <Home />
-    
+    <Container>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/sykkelcontainerfolie" element={<SykkelcontainerFolie />} />
+        <Route path="/sykkelcontainertrepanel" element={<SykkelcontainerTrepanel />} />
+        <Route path="/habitatpanel" element={<Habitatpanel />} />
+        <Route path="/trepanel" element={<Trepanel />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </Router>
     </>
   );
 }
---
+++ project-exam-2-lonsoien/src/components/about/About.js
@@ -0,0 +1,6 @@
+import React from 'react'; 
+import Heading from '../layout/Heading';
+
+export default function About() {
+  return <Heading title="Om Environment AS" />;
+}
