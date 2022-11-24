e8803245804c1fb8d28d1360a17780e774049bab
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-11-24 11:19:58 +0100

    images folder

--- project-exam-2-lonsoien/src/components/carousel/CarouselProjects.js
+++ project-exam-2-lonsoien/src/components/carousel/CarouselProjects.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import Carousel from 'react-bootstrap/Carousel';
-import headerimage from '../images/headerimage.png'; 
+import headerimage from '../../images/headerimage.png'; 
 
 function CarouselProjects() {
     return (
--- project-exam-2-lonsoien/src/components/header/Header.js
+++ project-exam-2-lonsoien/src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import headerimage from '../images/headerimage.png'; 
+import headerimage from '../../images/headerimage.png'; 
 
 
 function Header() {
