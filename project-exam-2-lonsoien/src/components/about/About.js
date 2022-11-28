3fd452891c10c36442520efb6be7184322079673
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-11-28 20:18:52 +0100

    profile

--- project-exam-2-lonsoien/src/components/about/About.js
+++ project-exam-2-lonsoien/src/components/about/About.js
@@ -2,7 +2,8 @@ import React from 'react';
 //import Container from 'react-bootstrap/esm/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import fafologo from '../../images/logo/fafologo.png'; 
+import Benedicte from '../../images/profile/Benedicte.jpeg';
+import Mathias from '../../images/profile/Mathias.jpeg';
 import Heading from '../layout/Heading';
 
 export default function About() {
@@ -23,20 +24,26 @@ export default function About() {
             <h3>Kontakt Personer</h3>
         </div>
         <Row>
-            <Col sm={6} style={{textAlign: "center", padding: "10px"}}>
+            <Col style={{textAlign: "center", padding: "10px"}}>
             <div className="profile-img">
-            <img style={{borderRadius: "50%", width: "200px", height: "200px"}}
-              src={fafologo}
+            <img style={{borderRadius: "20%", width: "240px", height: "310px"}}
+              src={Benedicte}
               alt="personlia contact"
             />
+            <h4 style={{fontSize: "22px", padding: "10px"}}>Benedicte</h4>
+            <p>Markedsansvarlig</p>
             </div>
             </Col>
-            <Col sm={6} style={{textAlign: "center", padding: "10px"}}>
+            <Col md="auto"></Col>
+            &nbsp;
+            <Col style={{textAlign: "center", padding: "10px"}}>
             <div className="profile-img">
-            <img style={{borderRadius: "50%", width: "200px", height: "200px"}}
-              src={fafologo}
+            <img style={{borderRadius: "20%", width: "240px", height: "310px"}}
+              src={Mathias}
               alt="personlia contact"
             />
+            <h4 style={{fontSize: "22px", padding: "10px"}}>Mathias</h4>
+            <p>Salgsansvarlig</p>
             </div>
             </Col>
             </Row>
