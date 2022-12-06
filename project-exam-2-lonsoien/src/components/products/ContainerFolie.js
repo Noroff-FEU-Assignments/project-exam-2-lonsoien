9280a95faeac42f48030cbb05b369bfb41678c6b
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-12-06 11:06:20 +0100

    install axios packages

--- project-exam-2-lonsoien/src/components/products/ContainerFolie.js
+++ project-exam-2-lonsoien/src/components/products/ContainerFolie.js
@@ -20,10 +20,7 @@ function ControlledCarousel() {
           src={Sykkelfolie1}
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" style={{height: "700px"}}
@@ -31,23 +28,12 @@ function ControlledCarousel() {
           alt="Second slide"
         />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" style={{height: "700px"}}
           src={Sykkelfolie3}
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
--- project-exam-2-lonsoien/src/components/products/ContainerTrepanel.js
+++ project-exam-2-lonsoien/src/components/products/ContainerTrepanel.js
@@ -21,10 +21,6 @@ function ControlledCarousel() {
           src={Sykkelcontainer1}
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" style={{height: "700px"}}
@@ -32,23 +28,12 @@ function ControlledCarousel() {
           alt="Second slide"
         />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" style={{height: "700px"}}
           src={Sykkelcontainer4}
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
@@ -56,13 +41,6 @@ function ControlledCarousel() {
           src={Sykkelcontainer5}
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
