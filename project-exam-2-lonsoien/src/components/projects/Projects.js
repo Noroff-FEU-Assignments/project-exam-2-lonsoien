a5ed65c6db0d2d4e1d72c068cc37fe5a035f14e2
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-12-09 18:34:31 +0100

    projects page

--- project-exam-2-lonsoien/src/components/projects/Projects.js
+++ project-exam-2-lonsoien/src/components/projects/Projects.js
@@ -1,12 +1,59 @@
-import React from 'react'; 
-import Heading from '../layout/Heading';
+
+import { API_URL } from "../../constants/Api";
+//import Container from "react-bootstrap/esm/Container";
+import Heading from "../layout/Heading";
+import { useState, useEffect } from "react";
 
 
-export default function Projects() {
-    return (
-        <>
-      <Heading title="Våre Prosjekter" />
-      <p>tekst here</p>
-      </>
-      );
+function Projects() {
+ const [projects, setProject] = useState([]);
+ const [loading, setLoading] = useState(true);
+ const [error, setError] = useState(null);
+
+ useEffect(function () {
+  async function fetchData() {
+   try {
+    const response = await fetch(API_URL);
+
+    if (response.ok) {
+     const json = await response.json();
+     console.log(json);
+     setProject(json);
+    } else {
+     setError("An error occured");
     }
+   } catch (error) {
+    setError(error.toString());
+   } finally {
+    setLoading(false);
+   }
+  }
+  fetchData();
+ }, []);
+
+ if (loading) {
+  return <div>Loading...</div>;
+ }
+
+ if (error) {
+  return <div>ERROR: An error occured</div>;
+ }
+
+ return (
+        <>
+        <Heading title="Våre Prosjekter"/>
+         {projects.map(function (project) {
+          return <div className= "projects-page" key={project.id}>
+           <h1> {project.title.rendered} </h1>
+           <img className="d-block w-100" src={project.featured_media} alt="project-foto" /> 
+           <p>{project.content.rendered}</p>
+           <p>{project.date}</p>
+          </div>
+            ;
+         })}
+        </>
+       );
+      }
+
+
+export default Projects;
--- project-exam-2-lonsoien/src/constants/Api.js
+++ project-exam-2-lonsoien/src/constants/Api.js
@@ -2,4 +2,4 @@ export const BASE_URL = "https://project-evironment.no/wp-json/";
 
 export const TOKEN_PATH = "jwt-auth/v1/token"; 
 
-export const API_URL = "https://project-evironment.no/wp-json/wp/v2/posts?_embed"; 
+export const API_URL = "https://project-evironment.no/wp-json/wp/v2/posts"; 
